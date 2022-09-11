fca6b56788b4d014e2adcf9fc9ed1acb1528048f
Solowise130 <iniodusolomon@gmail.com>
2022-09-10 22:04:09 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,19 +2,17 @@
 /*
  * main - Prints the alphabetic in lower and upper case
  *
- * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
  * Return: Always (Success)
  */
 int main(void)
 {
 	char c;
-    for (c = 'a'; c <= 'z'; c++)
+for (c = 'a'; c <= 'z'; c++)
 {	    
 putchar(c);
 }
-    for (c = 'A'; c <= 'Z'; c++)
-{    
+for (c = 'A'; c <= 'Z'; c++)
+}   
 putchar(c);
 }
 putchar('\n');
