37d9df8e99052bd2632b4f3f9d10db46fe082a9c
Solowise130 <iniodusolomon@gmail.com>
2022-09-13 03:30:04 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Prints putchar
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+_putchar("_");
+_putchar("P");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a"); 
+_putchar("r");
+
+return (0);
+}
