1d4ef1ff82d2390ec6378917493cb5cf4177d480
Solowise130 <iniodusolomon@gmail.com>
2022-09-16 06:50:00 -0700

    OK

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,15 +12,11 @@ void print_number(int n)
 	{
 		n1 = -n;
 		_putchar('-');
+		num = -num;
 	}
-	else
-	{
-		n1 = n;
-	}
-
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
-}		
+		if (num > 9)
+		{
+			print_number(num / 10);
+		}
+		_putchar(num % 10 + '0');
+}														
