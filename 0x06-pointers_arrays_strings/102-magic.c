1416c54a54ae160cbd7c927ca518058e4af89c45
Solowise130 <iniodusolomon@gmail.com>
2022-09-22 05:13:31 -0700

    Fixed

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,14 +2,14 @@
 
 int main(void)
 {
-int n;
+iint n;
 int a[5];
 int *p;
 
 a[2] = 1024;
 p = &n;
   /**
-   * write your line of code here...
+   * main - write your line of code here...
    * Remember:
    * - you are not allowed to use a
    * - you are not allowed to modify p
